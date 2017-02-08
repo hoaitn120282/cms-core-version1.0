98cd4a6ba5f96d0d2a93006b629cf0896eff922f
unknown <hoaitn@qs104-lap.qsoft.com.vn>
2017-02-08 17:35:58 +0700

    Update new core

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Theme;
+use App\Modules\ContentManager\Models\Articles;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blog = Articles::where('post_type','post')->where('post_status','publish')->orderBy('id', 'desc')->paginate(10);
+        return view(Theme::frontpage(),['blog'=>$blog]);
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Facades\Theme;
+use App\Modules\ContentManager\Models\Articles;
+use App\Modules\ContentManager\Models\Terms;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $res  = array();
+        $data = Articles::where('post_type', 'post')->orderBy('id', 'desc')->get();
+
+        foreach ($data as $value) {
+            $res[] = [
+                'id'      => $value->id,
+                'excerpt' => $value->post_excerpt,
+                'title'   => $value->post_title,
+                'created' => $value->created_at,
+                'content' => $value->post_content,
+                'image'   => $value->getMetaValue('featured_img'),
+            ];
+        }
+        return response()->json(['datas' => $res]);
+        //return view(Theme::active().'.post.index');
+
+    }
+
+    public function view($slug)
+    {
+        $model = Articles::where("post_name", $slug)->firstOrFail();
+        return view(Theme::active() . '.post.view', ["model" => $model]);
+    }
+
+    public function category($slug)
+    {
+        $model = Terms::where("slug", $slug)->first();
+        print_r($model);
+
+        //return view(Theme::active().'.post.view',["model"=>$model]);
+    }
+}
